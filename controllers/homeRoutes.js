7b0f45104bd7a80df00e9aa89ec307ecef81b36f
Trenton Rhines <trhines94@gmail.com>
2021-07-09 13:34:59 -0700

    added session object and login/logout routing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,16 @@ const router = require('express').Router()
 const { User } = require('../models')
 
 router.get('/', (req, res)=>{
-    res.render('login-signup')
+    console.log(req.session)
+    if(req.session.logged_in){
+        res.render('dashboard')
+    } else {
+        res.render('login-signup')
+    }
 })
+
+// router.get('/dashboard', (req, res)=>{
+//     res.render('dashboard')
+// })
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,27 @@
-// const newUserName = document.getElementById('newUserName').value.trim()
-// const newEmail = document.getElementById('newEmail').value.trim()
-// const newPassword = document.getElementById('newPassword').value.trim()
+
 const submitBtn = document.getElementById('submitBtn')
 
-console.log("my body is ready")
-
-const createAccountHandler = async (event) => {
+const loginHandler = async (event)=>{
   event.preventDefault()
-  console.log("in fetch")
-  const newUserName = document.getElementById('userName').value.trim()
-  const newEmail = document.getElementById('email').value.trim()
-  const newPassword = document.getElementById('password').value.trim()
-
-  if (newEmail && newUserName && newPassword){
-    const response = await fetch('/api/users/signup',{
+  
+  const email = document.getElementById('email').value.trim()
+  const password = document.getElementById('password').value.trim()
+  if(email && password){
+    const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ newUserName, newEmail, newPassword }),
-      headers: { 'Content-Type': 'application/json' }
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
     
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
+    if(response.ok){
+      document.location.replace('/');
     }
-    console.log(response)
   }
 
 }
 
-// const loginHandler = async (event)=>{
-//   event.preventDefault()
-//   console.log("login func")
-  
-//   const email = document.getElementById('email').value.trim()
-//   const password = document.getElementById('password').value.trim()
-//   if(email && password){
-//     const response = await fetch('/api/users/login', {
-//       method: 'POST',
-//       body: JSON.stringify({ email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-//     console.log(response)
-//   }
+submitBtn.addEventListener('click', loginHandler)
 
-// }
-
-// submitBtn.addEventListener('click', loginHandler)
-submitBtn.addEventListener("click", createAccountHandler)
 
 
 
