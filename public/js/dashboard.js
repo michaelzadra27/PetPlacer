6b02f4febdea7db8786beaba3c5883717c0f9f4d
Trenton Rhines <trhines94@gmail.com>
2021-07-11 16:56:44 -0700

    test commit

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,22 +5,21 @@ const linkBtn = document.getElementById('linkAcctBtn')
 const searchBtn = document.getElementById('searchBtn')
 
 const searchUser = async (event)=> {
-const searchUser = async (event)=> {
     event.preventDefault()
-    const userEmail = document.getElementById('userEmail').value.trim()
-
-    const userFound = await fetch(`/api/users/search/${userEmail}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-    if(userFound){
-        console.log(userFound)
-        console.log(JSON.stringify(userFound))
-    }
-    else{console.log("no user found")}
-    }
+    fetch(`https://ow-api.com/v1/stats/pc/us/${user}/complete`)
+    .then(function (response) {
+      if (response.status !== 200) {
+        alert("error." + response.status);
+        location.reload()
+      } else {
+      }
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    });
 }
+
 
 
 searchBtn.addEventListener('click', searchUser)
