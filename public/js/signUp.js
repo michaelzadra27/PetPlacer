0c1cabb139b5158b7f142e720660b2bbe36fc523
Trenton Rhines <trhines94@gmail.com>
2021-07-11 16:32:24 -0700

    fixed login route bug

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -14,7 +14,7 @@ const createAccountHandler = async (event) => {
       });
       
       if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
